30e168b24d7d572e7e25725b5ff892eb2baf7a18
Akash Garg <akash@orientaloutsourcing.com>
2022-11-28 10:05:20 +0530

    ui

--- lib/screen/auth_screen.dart
+++ lib/screen/auth_screen.dart
@@ -1,8 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:my_flutter_app/screen/chat_screen.dart';
 
 class Authenticate extends StatefulWidget {
   const Authenticate({Key? key}) : super(key: key);
@@ -27,15 +25,16 @@ class _AuthenticateState extends State<Authenticate> {
         await auth.signInWithEmailAndPassword(
             email: _emailController.text, password: _passController.text).then((value) => user = value
         ).onError((error, stackTrace) {
-              showDialog(context: context, builder: (context)=>AlertDialog(title: Text('Please Check Your Credentials'),));
+          debugPrint(error.toString());
+              showDialog(context: context, builder: (context)=>const AlertDialog(title: Text('Please Check Your Credentials'),));
               throw 'Login Failed';
         });
         if(user!=null){
           // Navigator.push(context, MaterialPageRoute(builder: (context) => const ChatScreen()),);
-          showDialog(context: context, builder: (context)=>AlertDialog(title: Text('Login Successful'),actions: [
+          showDialog(context: context, builder: (context)=>AlertDialog(title: const Text('Login Successful'),actions: [
             TextButton(onPressed: (){
               Navigator.of(context).pop();
-            }, child: Text('ok'))
+            }, child: const Text('ok'))
           ],));
         }
       }
@@ -44,13 +43,13 @@ class _AuthenticateState extends State<Authenticate> {
             email: _emailController.text, password: _passController.text).then((value) =>
             FirebaseFirestore.instance.collection('people').doc(value.user!.uid).set({'email': value.user!.email})
         ).onError((error, stackTrace) {
-          print(error);
-          showDialog(context: context, builder: (context)=>AlertDialog(title: Text('Please Correctly enter your details'),));
+          //print(error);
+          showDialog(context: context, builder: (context)=>const AlertDialog(title: Text('Please Correctly enter your details'),));
           throw "SignUp failed";
         });
       }
 
-      print(user);
+      //print(user);
 
       if (validate) {
         _formkey.currentState!.save();
@@ -60,90 +59,110 @@ class _AuthenticateState extends State<Authenticate> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amberAccent,
-      body: Center(
-        child: Card(margin: EdgeInsets.all(20),
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10)),
-          child: Form(
-            key: _formkey,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  TextFormField(
-                    controller: _emailController,
-                    key: ValueKey('email'),
-                    validator: (value) {
-                      if (value!.isEmpty || !value.contains('@')) {
-                        return 'Please enter valid email';
-                      }
-                      return null;
-                    },
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      label: Text('email'),
-                      suffixIcon: Icon(Icons.email),
-                    ),
-                    textInputAction: TextInputAction.next,
-                  ),
-                  if(login == false)
+      backgroundColor: Colors.yellow,
+      body: Stack(
+        children: [
+          ClipPath(
+            clipper: Clipper(),
+            child: Container(
+              padding: EdgeInsets.all(MediaQuery.of(context).size.height*0.1),
+              height: MediaQuery.of(context).size.height * 0.75,
+              width: MediaQuery.of(context).size.width,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.purpleAccent, Colors.blueAccent],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  stops: [0.4, 0.7],
+                ),
+              ),
+              child: Text('LOGIN',textAlign: TextAlign.center,style: TextStyle(color: Colors.white,fontSize: 40)),
+            ),
+          ),
+          Center(
+            child: Card(margin: const EdgeInsets.all(20),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10)),
+            child: Form(
+              key: _formkey,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
                     TextFormField(
-                      key: ValueKey('username'),
-                      validator: (value){
-                        if(value!.isEmpty || value.length<4){
-                          return 'Add a meaningful username';
+                      controller: _emailController,
+                      key: const ValueKey('email'),
+                      validator: (value) {
+                        if (value!.isEmpty || !value.contains('@')) {
+                          return 'Please enter valid email';
                         }
                         return null;
                       },
-                      decoration: InputDecoration(
-                        label: Text('Username'),
-                        suffixIcon: Icon(Icons.person),
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
+                        label: Text('email'),
+                        suffixIcon: Icon(Icons.email),
                       ),
+                      textInputAction: TextInputAction.next,
                     ),
-                  TextFormField(
-                    controller: _passController,
-                    key: ValueKey('password'),
-                    validator: (val) {
-                      if (val!.isEmpty || val.length < 6) {
-                        return 'Please make a strong password';
-                      }
-                      return null;
-                    },
-                    obscureText: (visible == false) ? true : false,
-                    decoration: InputDecoration(
-                      label: Text('Password'),
-                      hintText: 'Enter Your Password',
-                      suffixIcon: IconButton(
-                        onPressed: () {
-                          setState(() {
-                            visible = !visible;
-                          });
+                    if(login == false)
+                      TextFormField(
+                        key: const ValueKey('username'),
+                        validator: (value){
+                          if(value!.isEmpty || value.length<4){
+                            return 'Add a meaningful username';
+                          }
+                          return null;
                         },
-                        icon: (visible == false)
-                            ? Icon(Icons.visibility_off)
-                            : Icon(Icons.visibility),
+                        decoration: const InputDecoration(
+                          label: Text('Username'),
+                          suffixIcon: Icon(Icons.person),
+                        ),
+                      ),
+                    TextFormField(
+                      controller: _passController,
+                      key: const ValueKey('password'),
+                      validator: (val) {
+                        if (val!.isEmpty || val.length < 6) {
+                          return 'Please make a strong password';
+                        }
+                        return null;
+                      },
+                      obscureText: (visible == false) ? true : false,
+                      decoration: InputDecoration(
+                        label: const Text('Password'),
+                        hintText: 'Enter Your Password',
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              visible = !visible;
+                            });
+                          },
+                          icon: (visible == false)
+                              ? const Icon(Icons.visibility_off)
+                              : const Icon(Icons.visibility),
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 15,),
-                  ElevatedButton(key: ValueKey('login'),onPressed: Submit,
-                      child: (login == true) ? Text('Login') : Text('SignUp')),
-                  TextButton(key: ValueKey('Create'),onPressed: () {
-                    setState(() {
-                      login = !login;
-                    });
-                  },
-                      child: (login == true)
-                          ? Text('Create New Account')
-                          : Text('I already have an account'))
-                ],
+                    const SizedBox(height: 15,),
+                    ElevatedButton(key: const ValueKey('login'),onPressed: Submit,
+                        child: (login == true) ? const Text('Login') : const Text('SignUp')),
+                    TextButton(key: const ValueKey('Create'),onPressed: () {
+                      setState(() {
+                        login = !login;
+                      });
+                    },
+                        child: (login == true)
+                            ? const Text('Create New Account')
+                            : const Text('I already have an account'))
+                  ],
+                ),
               ),
             ),
-          ),
         ),
-      ),
+          ),
+      ]),
     );
   }
 }
@@ -152,38 +171,27 @@ class _AuthenticateState extends State<Authenticate> {
 //         body: Stack(
 //           children: [
 //           ClipPath(
-//             child: Container(
-//               height: MediaQuery.of(context).size.height * 0.7,
-//               //width: MediaQuery.of(context).size.width * 0.9,
-//               decoration: BoxDecoration(
-//                 borderRadius: BorderRadius.only(
-//                   bottomRight: Radius.elliptical(100, 100),
-//
-//                 ),
-//                 gradient: const LinearGradient(
-//                   colors: [Colors.purpleAccent, Colors.blueAccent],
-//                   begin: Alignment.topLeft,
-//                   end: Alignment.bottomRight,
-//                   stops: [0.4, 0.7],
-//                 ),
-//               ),
-//             ),
-//           ),
-//   ]
+//             child:
+//            ),
+// //   ]
 //         )
 //     );
 //   }
 // }
-// class Clipper extends CustomClipper<Path>{
-//   @override
-//   Path getClip(Size size) {
-//     Path path=Path();
-//   }
-//
-//   @override
-//   bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
-//     // TODO: implement shouldReclip
-//     throw UnimplementedError();
-//   }
-// }
-//);
+class Clipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.moveTo(0, size.height*0.2);
+     path.lineTo(0, size.height-50);
+    // path.lineTo(size.width, size.height);
+     path.quadraticBezierTo(size.width*0.5, size.height-100, size.width, size.height);
+     path.lineTo(size.width, 0);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return true;
+  }
+}
